c5b7d2bdc0ca111bc63688983e22bfd23faaa004
Lianzi <lianziliu.sugar@gmail.com>
2022-11-15 09:31:00 -0500

    add bg and btn filter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 function Navbar (){
     return (
-        <nav className="navbar"> 
+        <nav className="navbar">
             <a href="/">Home</a>
             <a href="/">About</a>
             <a href="/">Compatibility</a>
             <a href="/">Reviews</a>
-            <button>LogIn</button>
+            <button id="loginBtn" >LogIn</button>
         </nav>
     )
 
