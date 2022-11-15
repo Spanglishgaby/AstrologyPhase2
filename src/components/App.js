a668eb46b5ac80852d8df4765baee7c19e4f443c
Yuriko <ygabriela.arnott@gmail.com>
2022-11-15 09:55:46 -0500

    router install

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header'
 import PageContainer from './PageContainer';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header /> 
       <PageContainer/>
-    </div>
+    </Router>
   );
 }
 
